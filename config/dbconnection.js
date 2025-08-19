4dd6afcd042a1d4450bd89ec5a2c2384d874e708
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2025-08-19 14:58:50 +0530

    updated dbconnection.js

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -20,7 +20,7 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require("dotenv").config();
 
-const sequelize = new Sequelize('mycontacts', process.env.DB_USER, process.env.DB_PASSWORD,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
         dialect: 'mysql',
